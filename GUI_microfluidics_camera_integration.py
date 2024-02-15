84eec481c2d3a0487de1b48911447553ff985c6a
Syd Rawat <62871230+sid6155330@users.noreply.github.com>
2024-02-15 12:58:42 +1100

    Update GUI_microfluidics_camera_integration.py

--- GUI_microfluidics_camera_integration.py
+++ GUI_microfluidics_camera_integration.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+__author__ = "Siddharth Rawat"
+__copyright__ = ""
+__credits__ = ["None"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Siddharth Rawat, Shuji Kojima, Adam Micolich"
+__email__ = "siddharth.rawat@unsw.edu.au"
+__Laste updated on__ = "15 Feb. 2024"
+__status__ = "Production"
+
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk 
